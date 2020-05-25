980758183b952a969d84404a283d3ba204f5dae5
ReliableCauliflower <ostashko4@gmail.com>
2020-05-25 19:53:22 +0300

    Base interface
    Signed-off-by: ReliableCauliflower <ostashko4@gmail.com>

--- src/main/java/ua/nure/moleculis/emulator/Main.java
+++ src/main/java/ua/nure/moleculis/emulator/Main.java
@@ -14,21 +14,42 @@ public class Main {
         final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         boolean loggedIn = false;
+        boolean working = true;
         String accessToken = null;
 
         while (!loggedIn) {
-            print("Your login: ");
-            var login = in.readLine();
-            print("Your password: ");
-            var password = in.readLine();
+            println("Logging in...");
+            final String login = "stalker";
+            final String password = "Stalkerkryt1";
             accessToken = moleculisClient.login(login, password).getToken();
             if (accessToken != null) {
                 loggedIn = true;
             } else {
-                println("Log in failed, please try again.");
+                println("Log in failed, retrying...");
             }
         }
-        print("Your token: " + accessToken);
+        while (working) {
+            try {
+                println("1 - Verify token");
+                println("0 - Exit");
+
+                final int result = Integer.parseInt(in.readLine());
+                print(result);
+                switch (result) {
+                    case 0:
+                        working = false;
+                        break;
+                    case 1:
+                        println("\nVerifying Token...");
+                        break;
+                    default:
+                        println("\nUnknown command");
+                        break;
+                }
+            } catch (Exception e) {
+                println("Wrong input");
+            }
+        }
     }
 
     public static void print(Object message) {
